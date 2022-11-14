75e5829416958a03bb0926161d18f81ecc30eeaa
Adityo Arya Putra <adityo.arya.ft21@mail.umy.ac.id>
2022-11-14 17:09:09 +0700

    add Node previous, current

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -58,6 +58,9 @@ namespace double_linked_list
             }
             // if the node is to be inserted at between two node
             Node previous, current;
+            for (current = previous = START;
+                current != null && nim >= current.noMhs;
+                previous = current, current = current.next)
         }
     }
     internal class Program
