9b912d399d4c196631a93b8aff099c89e152390e
Adityo Arya Putra <adityo.arya.ft21@mail.umy.ac.id>
2022-11-14 19:27:55 +0700

    add case 6 in classs program

--- double_linked_list/Program.cs
+++ double_linked_list/Program.cs
@@ -237,6 +237,13 @@ namespace double_linked_list
                                 }
                             }
                             break;
+                        case '6':
+                            return;
+                        default:
+                            {
+                                Console.WriteLine("\nInvalid option");
+                            }
+                            break;
                     }
                 }
             }
